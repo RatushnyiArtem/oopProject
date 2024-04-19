11f7cb5f8bfeca40e7c00666369c9cc9a21c1b1c
RatushnyiArtem <92036528+RatushnyiArtem@users.noreply.github.com>
2024-04-19 12:42:20 +0200

    Create class Shop and modified class User by adding amountMoney

--- main.cpp
+++ main.cpp
@@ -5,15 +5,19 @@ class Soap{
     std::string color;
     std::string brand;
     double percentage;
+    double price;
 
     public:
-        Soap(std::string _color, std::string _brand, int _percentage = 100): color(_color), brand(_brand),percentage(_percentage) {};
+        Soap(std::string _color, std::string _brand, double _price, double _percentage = 100): color(_color), brand(_brand), price(_price), percentage(_percentage) {};
         virtual void use(){
-            percentage -=2;
+            percentage = percentage - percentage*0.012;
         }
         void soapLeft(){};
-        const int getPerc() const{
+        const double getPerc() const{
             return percentage;
+        }
+        const double getPrice() const{
+            return price;
         }
         virtual ~Soap() {}
 };
@@ -21,9 +25,10 @@ class Soap{
 class User{
     std::string name;
     std::string surname;
+    double moneyAmount;
     Soap *f;
     public:
-        User(const std::string _name, const std::string _surname, Soap *smth): name(_name), surname(_surname), f(smth){};
+        User(const std::string _name, const std::string _surname,double _money, Soap *smth): name(_name), surname(_surname),moneyAmount(_money), f(smth){};
         const std::string getName() const{
             return name;
         }
@@ -33,6 +38,22 @@ class User{
         void useSoap(){
             f->use();
         };
+        const double getMoney() const{
+            return moneyAmount;
+        }
+        
+};
+
+class Shop{
+    std::string name;
+    std::vector<Soap*> elements;
+    public:
+
+        ~Shop(){
+            for(auto *element : elements){
+                delete element;
+            }
+        }
 };
 
 class liquidSoap : public Soap{
@@ -49,9 +70,9 @@ class handSoap : public Soap{
 
 
 int main(){
-    Soap p ("red", "dove");
-    User m("fdfds", "slfksdf",&p);
+    Soap p ("red", "dove",45.0);
+    User m("fdfds", "slfksdf",1001.02,&p);
     m.useSoap();
-    std::cout<<p.getPerc();
-    
+    std::cout<<p.getPerc()<<std::endl;
+    std::cout<<p.getPrice();
 }
